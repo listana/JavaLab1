54ce44258830f18e79759e974dfe9c39a5d94ca3
listana <47680964+listana@users.noreply.github.com>
2020-03-12 17:27:43 +0300

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,18 @@
+package homework1;
+
+public class Rectangle {
+
+    Rectangle(String s, int h, int w) {
+
+
+    }
+
+    String Name;
+    int Height;
+    int Width;
+
+    // реализовать метод который рассчитывает площадь и выводит в консоль
+    void printArea() {
+        System.out.println(Height * Width);
+    }
+}
